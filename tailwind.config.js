45996d496265328bb5356520b92aa360ff421b3f
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2024-12-23 22:44:18 +0530

    feat: font style
    | Font   | Usage    |
    |--------|--------------|
    | Rubik  | Headings     |
    | Roboto | Body Text    |
    | Poppins| Accent Text  |

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
@@ -6,6 +7,19 @@ export default {
         primary: "#050816",
         secondary: "#aaa6c3",
         tertiary: "#151030",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      screens: {
+        xs: "375px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
       zIndex: {
         1000: "1000",
