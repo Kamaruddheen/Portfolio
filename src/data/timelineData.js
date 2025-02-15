0db771611fe9ccd0714cec945260bfcae2567f5d
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2025-02-15 15:57:02 +0530

    add contract developer data in Journey

--- src/data/timelineData.js
+++ src/data/timelineData.js
@@ -1,10 +1,23 @@
-import { FaBriefcase, FaGraduationCap } from "react-icons/fa";
+import { FaBriefcase, FaGraduationCap } from "react-icons/fa"
 
 export const timelineData = [
   {
-    title: "Pesto Fellowship",
+    title: "Mern Stack Developer | Contract",
+    subtitle: "Stealth Startup",
+    date: "Oct 2024 - Present",
+    para1:
+      "I had the chance to work closely with the founder of a small startup, where I played a hands-on role in building a product from scratch 🚀. I dove into the MERN stack (MongoDB, Express.js, React, Node.js) to develop both front-end and back-end solutions, which really helped me grow my technical skills 💻. The startup environment was fast-paced, and I quickly found myself tackling complex problems, all while contributing to key product features that improved both performance and user experience ⚙️.",
+    para2:
+      "Along the way, I gained invaluable problem-solving skills, especially when critical issues arose under tight deadlines ⏱️. I learned to adapt quickly to new technologies and apply them in real-world scenarios, all of which contributed to my professional growth 📈. As my contract is set to conclude in February 2025, I look back at this experience as a pivotal chapter in my career 📅.",
+    description:
+      "I had the chance to work closely with the founder of a small startup, where I played a hands-on role in building a product from scratch 🚀. I dove into the MERN stack (MongoDB, Express.js, React, Node.js) to develop both front-end and back-end solutions, which really helped me grow my technical skills 💻. The startup environment was fast-paced, and I quickly found myself tackling complex problems, all while contributing to key product features that improved both performance and user experience ⚙️. Along the way, I gained invaluable problem-solving skills, especially when critical issues arose under tight deadlines ⏱️. I learned to adapt quickly to new technologies and apply them in real-world scenarios, all of which contributed to my professional growth 📈. As my contract is set to conclude in February 2025, I look back at this experience as a pivotal chapter in my career 📅.",
+    icon: FaBriefcase,
+    type: "work"
+  },
+  {
+    title: "Mern Stack Developer | Fellowship",
     subtitle: "Full Stack Development and Product Engineering",
-    date: "Dec 2023 - Sep 2024",
+    date: "Dec 2023 - Oct 2024",
     para1:
       "I decided to leave my job at Infosys to explore my passion for web development, even though the learning and future opportunities seemed a bit restricted at the time. 🚀 I took a risk to pursue my dream and dive into MERN stack development, which turned out to be an exciting and fulfilling adventure. 💻 Along the way, I met a lot of amazing people and learned so much from them. 🤝",
     para2:
@@ -12,7 +25,7 @@ export const timelineData = [
     description:
       "I decided to leave my job at Infosys to explore my passion for web development, even though the learning and future opportunities seemed a bit restricted at the time. I took a risk to pursue my dream and dive into MERN stack development, which turned out to be an exciting and fulfilling adventure. Along the way, I met a lot of amazing people and learned so much from them. By the end of the program, we even built an awesome project, WorkoutWings. This experience helped me understand the true power of full-stack development, from creating responsive front-end interfaces with React to building scalable back-end systems with Node.js and MongoDB.",
     icon: FaGraduationCap,
-    type: "education",
+    type: "education"
   },
   {
     title: "Senior System Associate",
@@ -25,7 +38,7 @@ export const timelineData = [
     description:
       "My 1st job was truly special! At Infosys, I worked with an amazing team, helping to build robust enterprise-level software solutions. Not only did I gain tons of experience in system design and project management, but I also had the chance to share my tech knowledge with my colleagues. In fact, I even taught some of my coding skills to others at some point! It was an exciting time, contributing to cutting-edge tech while growing as both a developer and a mentor.",
     icon: FaBriefcase,
-    type: "work",
+    type: "work"
   },
   {
     title: "BSc Computer Science",
@@ -38,7 +51,7 @@ export const timelineData = [
     description:
       "Earned my Bachelor’s degree in Computer Science with a focus on programming and software development. It was a rollercoaster of coding challenges, problem-solving, and growing a love for all things tech! Along the way, I took a short trip to a cool serene place where I could recharge while riding my bike and capturing the beauty through photography. It was my way of balancing academics with my dream and passion for tech, while also indulging in my love for photography during those refreshing getaways.",
     icon: FaGraduationCap,
-    type: "education",
+    type: "education"
   },
   {
     title: "Advanced Programme in Full Stack Software Engineering",
@@ -51,7 +64,7 @@ export const timelineData = [
     description:
       "Soon after my internship, I joined this program to channel all my energy into honing my skills further and to take on bigger challenges in full-stack development. I dived deep into Core Java and learned advanced concepts that surprisingly became my favorite part of the journey! I discovered a love for Java while mastering how to build full-fledged apps that not only look great but also run smoothly and efficiently.",
     icon: FaGraduationCap,
-    type: "education",
+    type: "education"
   },
   {
     title: "Python Developer",
@@ -64,8 +77,8 @@ export const timelineData = [
     description:
       "This internship was the moment everything clicked. On my third day, I realized that what we learned in college was just the beginning, and the real world was a whole different ball game. It was an eye-opening experience that pushed me to go beyond the textbooks. After the internship ended, I became incredibly self-motivated, diving into self-learning and embracing challenges. This was the turning point in my coding journey, where I truly fell in love with problem-solving and started growing as a developer.",
     icon: FaBriefcase,
-    type: "work",
-  },
+    type: "work"
+  }
 ].sort(
   (a, b) => new Date(b.date.split(" - ")[0]) - new Date(a.date.split(" - ")[0])
-);
+)
